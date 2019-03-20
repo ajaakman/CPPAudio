d35604887f296ac943a7b985959a8484910dbdd1
Artur Jaakman <jaakmanartur@gmail.com>
2019-03-20 14:27:46 +0200

    Playing Wav File.

--- src/main.cpp
+++ src/main.cpp
@@ -26,14 +26,73 @@
 
 #include <iostream>
 #include <SDL.h>
+
+#define FILE_PATH "./res/audio/testClip.wav"
+
+struct AudioData
+{
+	Uint8* pos;
+	Uint32 length;
+};
+
+void MyAudioCallback(void* userdata, Uint8* stream, int streamLength)
+{
+	AudioData* audio = (AudioData*)userdata;
+
+	if (audio->length == 0)
+	{
+		return;
+	}
+
+	Uint32 length = (Uint32)streamLength;
+	length = (length > audio->length ? audio->length : length);
+
+
+	SDL_memcpy(stream, audio->pos, length);
+
+	audio->pos += length;
+	audio->length -= length;
+}
+
 #undef main
 int main(int argc, char** argv)
 {
 	// Just to make sure SDL is working
-	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Init(SDL_INIT_AUDIO);
 
-	std::cout << "Hello World!" << std::endl;
+	SDL_AudioSpec wavSpec;
+	Uint8* wavBuffer;
+	Uint32 wavLength;
 
+	if (SDL_LoadWAV(FILE_PATH, &wavSpec, &wavBuffer, &wavLength) == NULL)
+		std::cout << "Error: " << FILE_PATH << " cound not be loaded as an audio file" << std::endl;
+		
+
+	AudioData audio;
+	audio.pos = wavBuffer;
+	audio.length = wavLength;
+
+	//SDL_memset(&wavSpec, 0, sizeof(wavSpec));
+	wavSpec.userdata = &audio;
+	//wavSpec.freq = 41000;
+	//wavSpec.format = AUDIO_F32;
+	//wavSpec.channels = 2;
+	//wavSpec.samples = 4096;
+	wavSpec.callback = MyAudioCallback;
+
+	SDL_AudioDeviceID device = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
+
+	if (device == 0)	
+		std::cout << SDL_GetError() << " | Could not open audio device \n";
+	
+	//int success = SDL_QueueAudio(device, wavStart, wavLength);
+	SDL_PauseAudioDevice(device, 0);
+
+	while (audio.length > 0)
+		SDL_Delay(100);
+
+	SDL_CloseAudioDevice(device);
+	SDL_FreeWAV(wavBuffer);
 	SDL_Quit();
 	return 0;
 }
